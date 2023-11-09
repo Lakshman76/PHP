e8231b623f35be99ec3f4b8764e8d1c90d5d4c88
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-11-09 16:43:47 +0000

    uppercase and lowercase in php

---
+++ String/upper_lowercase.php
@@ -0,0 +1,17 @@
+<?php
+
+  $str = "LakshMan KumaR";
+
+  echo strtoupper($str)."<br>"; // LAKSHMAN KUMAR
+
+  echo strtolower($str)."<br>"; // lakshman kumar
+
+  echo lcfirst($str)."<br>"; // lakshMan KumaR
+
+  $str2 = "hello everyone pay attention please!";
+
+  echo ucfirst($str2)."<br>"; // Hello everyone pay attention please!
+
+  echo ucwords($str2)."<br>"; // Hello Everyone Pay Attention Please!
+
+?>
