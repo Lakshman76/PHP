55b380aaec6acef4e09061850a60046b41579f1e
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-11-09 16:52:21 +0000

    Different split method

--- String/split.php
+++ String/split.php
@@ -3,13 +3,21 @@
     // str_split() 
 
     $str = "Lakshman";
-    $arr = str_split($str, 2);
-    print_r($arr); // Array ( [0] => La [1] => ks [2] => hm [3] => an )
+    $arr = str_split($str);
+    print_r($arr); // Array ( [0] => L [1] => a [2] => k [3] => s [4] => h [5] => m [6] => a [7] => n )
+
+    echo "<br>";
+
+    $arr2 = str_split($str, 2);
+    print_r($arr2); // Array ( [0] => La [1] => ks [2] => hm [3] => an )
+
+    echo "<br>";
 
     // chunk_split()
 
     $str2 = "Lakshman Kumar";
     $newStr = chunk_split($str2, 2, ".."); // La..ks..hm..an.. K..um..ar..
-    echo $newStr;
+    echo $newStr."<br>";
+
 
 ?>
