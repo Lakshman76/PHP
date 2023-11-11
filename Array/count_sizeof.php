b2d2b449889747371b12296205d67e522d7adf92
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-11-11 15:59:40 +0000

    count and sizeof function in php

---
+++ Array/count_sizeof.php
@@ -0,0 +1,13 @@
+<?php
+
+  // count() 
+  $color = array('red', 'green', 'blue', 'yellow');
+  echo count($color)."<br>"; // 4
+
+  // sizeof()
+  $superHeros = array('Spiderman', 'Ironman', 'Thor', 'Hulk', 'Dr. Strange');
+  echo sizeof($superHeros)."<br>"; // 5
+
+  # count and sizeof are approx same in php.
+
+?>
